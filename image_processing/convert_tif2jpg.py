144b6d55ea0a9769784062ce52337f40acb19aa2
Dorian Tsai <dorian.tsai@gmail.com>
2023-04-05 15:47:37 +1000

    first commit for conda env script and convert tiff to jpg

---
+++ image_processing/convert_tif2jpg.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+""" convert_tiff2jpg.py
+"""
+
+from PIL import Image
+import os
+
+# tiff directory
+tif_dir = '/home/dorian/Data/cgras_dataset_20230403/tif'
+
+
+# jpg out directory
+jpg_dir = '/home/dorian/Data/cgras_dataset_20230403/jpg'
+os.makedirs(jpg_dir, exist_ok=True)
+
+for i, filename in enumerate(os.listdir(tif_dir)):
+    if filename.endswith('.tif'):
+        print(f'converting {i}: {filename}')
+        # open TIFF image
+        image = Image.open(os.path.join(tif_dir, filename))
+        # convert TIFF to jpeg
+        new_filename = filename.rsplit('.', 1)[0] + '.jpg'
+        image.convert('RGB').save(os.path.join(jpg_dir, new_filename))
+
+        image.close()
+
+print('Conversion complete')
---
+++ make_cgras_venv.sh
@@ -0,0 +1,4 @@
+# script to automatically make conda environment defined in cgras.yml
+mamba env create -f cgras.yml
+
+# conda activate cgras
