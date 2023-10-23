bf039426711b50c59e66fa981457206800e8fbf1
Java Terry <33javalava@gmail.com>
2023-10-23 10:28:24 +1000

    java working train code

--- annotation/cvatcoco_to_yolo.py
+++ annotation/cvatcoco_to_yolo.py
@@ -1,6 +1,6 @@
 from ultralytics.data.converter import convert_coco
 
-convert_coco(labels_dir='/home/java/Downloads/cgras_20231810_polygon_coco/annotations/',
+convert_coco(labels_dir='/home/java/Java/data/cgras_20230421/labels/annotations/',
                  use_segments=True, use_keypoints=False, cls91to80=False)
 
 # will save txt files in source dir of where python script is called + '/yolo_labels/labels/default/'
--- segmenter/train_segmenter.py
+++ segmenter/train_segmenter.py
@@ -11,19 +11,25 @@ import torch
 
 # load model
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-model = YOLO('home/java/Java/cgras/cgras_settler_counter/yolov8x-seg.pt')
+model = YOLO('/home/java/Java/Cgras/cgras_settler_counter/yolov8x-seg.pt').to(device)
 
 # train model
-data_file = '/home/java/Java/cgras/cgras_settler_counter/segmenter/cgras_20230421.yaml'
+# for base training only want, 0: 'recruit_live_white', 1: 'recruit_cluster_live_white', 
+# 6: 'recruit_dead', 7: 'recruit_cluster_dead', 9: 'pest_tubeworm'
+data_file = '/home/java/Java/Cgras/cgras_settler_counter/segmenter/cgras_20230421.yaml'
 #model.train(data=data_file, epochs=200, batch=10)
-model.train(data=data_file, epochs=10, batch=10)
+# classes arg is lightweight and simply ignore classes that are not included in the classes list, 
+# during train, val and predict, it has no effect on model architecture.
+model.train(data=data_file, epochs=1, batch=5, classes=[0,1,6,7,9])
 
 print('Model Inference:')
-image_file = '/home/java/Java/data/cgras_dataset_20230421/train/images/00_20230116_MIS1_RC_Aspat_T04_08.jpg'
-results = model(image_file)
+image_file = '/home/java/Java/data/cgras_20230421/train/images/00_20230116_MIS1_RC_Aspat_T04_08.jpg'
+results = model.detect(source=image_file)
 print(results)
 
-
+# Free up GPU memory
+#del model
+#torch.cuda.empty_cache()
 
 # for interactive debugger in terminal:
 import code
