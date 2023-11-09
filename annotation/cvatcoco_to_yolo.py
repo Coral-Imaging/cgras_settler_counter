3a6ab94a02d683f86d021ae754ee9930195555d8
Java Terry <33javalava@gmail.com>
2023-11-09 15:53:01 +1000

    added file manipulation

--- annotation/cvatcoco_to_yolo.py
+++ annotation/cvatcoco_to_yolo.py
@@ -1,7 +1,27 @@
 from ultralytics.data.converter import convert_coco
+import os
+import zipfile
+import shutil
 
-convert_coco(labels_dir='/home/java/Java/data/cgras_20230421/labels/annotations/',
+#export from CVAT as COCO1.1
+#comes in as ziped file with annotations/instances_default.json
+file_name = 'coco_5_images_polygons.zip'
+save_dir = '/home/java/Java/data/new_data'
+download_dir = '/home/java/Downloads'
+
+with zipfile.ZipFile(os.path.join('/home/java/Downloads', file_name), 'r') as zip_ref:
+   zip_ref.extractall('/home/java/Downloads')
+    #annotation folder in downloads
+
+downloaded_labels_dir = os.path.join(download_dir, 'annotations')
+lable_dir = os.path.join(save_dir, 'labels')
+coco_labels = os.path.join(lable_dir, 'default')
+
+convert_coco(labels_dir=downloaded_labels_dir, save_dir=save_dir,
                  use_segments=True, use_keypoints=False, cls91to80=False)
 
-# will save txt files in source dir of where python script is called + '/yolo_labels/labels/default/'
-# ie. home/java/Java/cgras/cgras_settler_counter/yolo_labels/labels/default
+for filename in os.listdir(coco_labels):
+    source = os.path.join(coco_labels, filename)
+    destination = os.path.join(lable_dir, filename)
+    if os.path.isfile(source):
+        shutil.move(source, destination)
