23ef07d812653de1773f9fab1ac02334456bb4b8
CaptainReginald <Rwardleworth@outlook.com>
2024-11-19 13:22:02 +1000

    Config for Reg PC

--- segmenter/train_segmenter.py
+++ segmenter/train_segmenter.py
@@ -7,12 +7,13 @@ train basic yolov8 model for image segmentation
 from ultralytics import YOLO
 import torch
 
-data_file = '/home/java/Java/Cgras/cgras_settler_counter/segmenter/cgras_20230421.yaml'
+data_file = '/home/wardlewo/Reggie/corals/cgras_settler_counter/segmenter/cgras_20230421.yaml'
 #data_file = sys.argv[1]
 
 # load model
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-model = YOLO('/home/java/Java/ultralytics/runs/segment/train9/weights/cgras_yolov8n-seg_640p_20231209.pt').to(device)
+model = YOLO('yolov8x-seg.pt').to(device)
+             #/home/java/Java/ultralytics/runs/segment/train9/weights/cgras_yolov8n-seg_640p_20231209.pt').to(device)
 
 # train model
 # for base training only want, 0: 'recruit_live_white', 1: 'recruit_cluster_live_white', 
@@ -22,7 +23,7 @@ model = YOLO('/home/java/Java/ultralytics/runs/segment/train9/weights/cgras_yolo
 
 # classes arg is lightweight and simply ignore classes that are not included in the classes list, 
 # during train, val and predict, it has no effect on model architecture.
-model.train(data=data_file, epochs=500, batch=1,  classes=[0,1,2,3,6,7,8,9]) #test run
+model.train(data=data_file, epochs=1, batch=1,  classes=[0,1,2,3,6,7,8,9]) #test run
 #model.train(data=data_file, epochs=100, batch=-1, classes=[0,1,6,7,9,10], imgsz=1280, nbs=12)
 
 # print('Model Inference:')
