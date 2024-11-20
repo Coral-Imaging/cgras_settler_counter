419be729ae589bcdf50316fa2163e2096d96f98c
Java Terry <33javalava@gmail.com>
2024-11-21 08:56:20 +1000

    hpc_update

--- segmenter/train_segmenter.py
+++ segmenter/train_segmenter.py
@@ -7,12 +7,13 @@ train basic yolov8 model for image segmentation
 from ultralytics import YOLO
 import torch
 
-data_file = '/home/java/Java/Cgras/cgras_settler_counter/segmenter/cgras_20230421.yaml'
+data_file = '/home/terryj/cgras_settler_counter/segmenter/cgras_hpc.yaml'
+weights = '/home/terryj/runs/train13/weights/last.pt'
 #data_file = sys.argv[1]
 
 # load model
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-model = YOLO('/home/java/Java/ultralytics/runs/segment/train9/weights/cgras_yolov8n-seg_640p_20231209.pt').to(device)
+model = YOLO(weights).to(device)
 
 # train model
 # for base training only want, 0: 'recruit_live_white', 1: 'recruit_cluster_live_white', 
@@ -22,8 +23,11 @@ model = YOLO('/home/java/Java/ultralytics/runs/segment/train9/weights/cgras_yolo
 
 # classes arg is lightweight and simply ignore classes that are not included in the classes list, 
 # during train, val and predict, it has no effect on model architecture.
-model.train(data=data_file, epochs=500, batch=1,  classes=[0,1,2,3,6,7,8,9]) #test run
-#model.train(data=data_file, epochs=100, batch=-1, classes=[0,1,6,7,9,10], imgsz=1280, nbs=12)
+model.train(data=data_file, epochs=500, batch=13, 
+            project='runs', save_period=5,
+            imgsz=480,
+            cache=True,
+            workers = 8)
 
 # print('Model Inference:')
 # image_file = '/home/java/Java/data/cgras_20230421/train/images/00_20230116_MIS1_RC_Aspat_T04_08.jpg'
