f4fd933546546c5a61eb45bae65f31f620cff174
Java Terry <33javalava@gmail.com>
2025-01-09 15:40:31 +1000

    fixed reading error

--- image_processing/tiling_images.py
+++ image_processing/tiling_images.py
@@ -11,8 +11,36 @@ import glob
 from PIL import Image
 from shapely.geometry import Polygon, box, MultiPolygon, GeometryCollection
 from shapely.validation import explain_validity
+from tqdm import tqdm
 
-from annotation.Utils import classes, class_colours 
+#from annotation.Utils import classes, class_colours 
+classes = ["recruit_live_white", "recruit_cluster_live_white", "recruit_symbiotic", "recruit_cluster_symbiotic", "recruit_partial",
+           "recruit_cluster_partial", "recruit_dead", "recruit_cluster_dead", "grazer_snail", "pest_tubeworm", "unknown"] #how its in cvat
+# Colours for each class
+orange = [255, 128, 0] 
+blue = [0, 212, 255] 
+purple = [170, 0, 255] 
+yellow = [255, 255, 0] 
+brown = [144, 65, 2] 
+green = [0, 255, 00] 
+red = [255, 0, 0]
+cyan = [0, 255, 255]
+dark_purple =  [128, 0, 128]
+light_grey =  [192, 192, 192] 
+dark_green = [0, 100, 0] 
+class_colours = {classes[0]: blue,
+                classes[1]: green,
+                classes[2]: purple,
+                classes[3]: yellow,
+                classes[4]: brown,
+                classes[5]: cyan,
+                classes[6]: orange,
+                classes[7]: red,
+                classes[8]: dark_purple,
+                classes[9]: light_grey,
+                classes[10]: dark_green}
+
+
 full_res_dir = '/media/java/CGRAS-SSD/cgras_data_copied_2240605/samples/dec_17_split/train/'
 save_path = '/media/java/CGRAS-SSD/cgras_data_copied_2240605/samples/dec_17_split_n_tilled/'
 
@@ -24,8 +52,8 @@ prefix = 'train'
 #images in one folder, labels in another. Only want to do images with an ossociated label file
 imglist = sorted(glob.glob(os.path.join(full_res_dir, 'images', '*.jpg')))
 TILE_OVERLAP = round((TILE_HEIGHT+TILE_WIDTH)/2 * TRUNCATE_PERCENT)
-directory_count = 0
-
+directory_count = 4
+file_counter = 0   
 
 def make_sub_dirctory_save(prefix, save_path):
     save_train = os.path.join(save_path, f'{prefix}_{directory_count}')
@@ -94,11 +122,19 @@ def cut_n_save_img(x_start, x_end, y_start, y_end, np_img, img_save_path):
 def cut_annotation(x_start, x_end, y_start, y_end, lines, imgw, imgh):
     """From instance lines in label file, find objects in the bounding box and return the renormalised xy points if there are any"""
     writeline = []
-    for line in lines:
+    incomplete_lines = set()  
+    for j, line in enumerate(lines):
         parts = line.split()
         class_number = int(parts[0])
         polygon = create_polygon_unnormalised(parts, imgw, imgh)
 
+        if len(parts) < 1 or polygon.is_empty:
+            if j not in incomplete_lines: 
+                print(f"line {j} is incomplete")
+                incomplete_lines.add(j)
+                import code
+                code.interact(local=dict(globals(), **locals()))    
+            continue
         if is_mostly_contained(polygon, x_start, x_end, y_start, y_end, TRUNCATE_PERCENT):
             truncated_polygon = truncate_polygon(polygon, x_start, x_end, y_start, y_end)
             xyn = normalise_polygon(truncated_polygon, class_number, x_start, x_end, y_start, y_end, TILE_WIDTH, TILE_HEIGHT)
@@ -113,39 +149,49 @@ def cut(img_name, save_img, test_name, save_labels, txt_name, img_no):
     np_img = np.array(pil_img, dtype=np.uint8)
     img = cv.imread(img_name)
     imgw, imgh = img.shape[1], img.shape[0]
+    x_tiles = (imgw + TILE_WIDTH - TILE_OVERLAP - 1) // (TILE_WIDTH - TILE_OVERLAP)
+    y_tiles = (imgh + TILE_HEIGHT - TILE_OVERLAP - 1) // (TILE_HEIGHT - TILE_OVERLAP)
+    total_tiles = x_tiles * y_tiles  # Total tiles to process
+    with tqdm(total=total_tiles, desc=f"Processing image {os.path.basename(img_name)[:-4]}", unit="tile") as pbar:
+        for x in range(x_tiles):
+            for y in range(y_tiles):
+                x_end = min((x + 1) * TILE_WIDTH - TILE_OVERLAP * (x != 0), imgw)
+                x_start = x_end - TILE_WIDTH
+                y_end = min((y + 1) * TILE_HEIGHT - TILE_OVERLAP * (y != 0), imgh)
+                y_start = y_end - TILE_HEIGHT
+
+                img_save_path = os.path.join(save_img, f"{test_name}_{str(x_start).zfill(4)}_{str(y_start).zfill(4)}.jpg")
+                txt_save_path = os.path.join(save_labels, f"{test_name}_{str(x_start).zfill(4)}_{str(y_start).zfill(4)}.txt")
+                #make cut and save image
+                cut_n_save_img(x_start, x_end, y_start, y_end, np_img, img_save_path)
+                #cut annotaion and save
+                with open(txt_name, 'r') as file:
+                    lines = file.readlines()
+                try:
+                    writeline = cut_annotation(x_start, x_end, y_start, y_end, lines, imgw, imgh)
+                except:
+                    print("error in cut_annotations")
+                    import code
+                    code.interact(local=dict(globals(), **locals()))    
+
+                with open(txt_save_path, 'w') as file:
+                    for line in writeline:
+                        file.write(" ".join(map(str, line)).replace('[', '').replace(']', '').replace(',', '') + "\n")
+                pbar.update(1)
+                # import code
+                # code.interact(local=dict(globals(), **locals()))        
+
+def calculate_img_section_no(img_name):
+    """return the number of tiles made from one image depending on tile width and img size"""
+    pil_img = Image.open(img_name, mode='r')
+    np_img = np.array(pil_img, dtype=np.uint8)
+    img = cv.imread(img_name)
+    imgw, imgh = img.shape[1], img.shape[0]
     # Count number of sections to make
     x_tiles = (imgw + TILE_WIDTH - TILE_OVERLAP - 1) // (TILE_WIDTH - TILE_OVERLAP)
     y_tiles = (imgh + TILE_HEIGHT - TILE_OVERLAP - 1) // (TILE_HEIGHT - TILE_OVERLAP)
-    for x in range(x_tiles):
-        print(f"On image {i+1}, processing x_tile {x}/{x_tiles} and y_tile {y_tiles}")
-        for y in range(y_tiles):
-            x_end = min((x + 1) * TILE_WIDTH - TILE_OVERLAP * (x != 0), imgw)
-            x_start = x_end - TILE_WIDTH
-            y_end = min((y + 1) * TILE_HEIGHT - TILE_OVERLAP * (y != 0), imgh)
-            y_start = y_end - TILE_HEIGHT
+    return x_tiles*y_tiles
 
-            img_save_path = os.path.join(save_img, f"{test_name}_{str(x_start).zfill(4)}_{str(y_start).zfill(4)}.jpg")
-            txt_save_path = os.path.join(save_labels, f"{test_name}_{str(x_start).zfill(4)}_{str(y_start).zfill(4)}.txt")
-
-            #make cut and save image
-            cut_n_save_img(x_start, x_end, y_start, y_end, np_img, img_save_path)
-
-            #cut annotaion and save
-            with open(txt_name, 'r') as file:
-                lines = file.readlines()
-            try:
-                writeline = cut_annotation(x_start, x_end, y_start, y_end, lines, imgw, imgh)
-            except:
-                print("error in cut_annotations")
-                import code
-                code.interact(local=dict(globals(), **locals()))    
-
-            with open(txt_save_path, 'w') as file:
-                for line in writeline:
-                    file.write(" ".join(map(str, line)).replace('[', '').replace(']', '').replace(',', '') + "\n")
-            
-            # import code
-            # code.interact(local=dict(globals(), **locals()))        
 
 def visualise(imgdir, save_path):
     """Show all the annotations on to a set of cut images and save at save_path"""
@@ -191,19 +237,24 @@ def visualise(imgdir, save_path):
 
 max_files = 16382
 for i, img in enumerate(imglist):
-    if i < 314:
+    if i < 0:
         continue
-    if i > 0 and i+426 % max_files == 0:
-        directory_count += 1
-        save_img, save_labels = make_sub_dirctory_save(prefix, save_path)
-
     name = os.path.basename(img)[:-4]
     img_name = os.path.join(full_res_dir,'images', name+'.jpg')
     txt_name = os.path.join(full_res_dir,'labels', name+'.txt')
+
+    files_to_add = calculate_img_section_no(img_name)
+    if file_counter+files_to_add >= max_files:
+        print(f'Directory full, as {file_counter} files already made and {files_to_add} will be made with next image')
+        directory_count += 1
+        file_counter = 0
+        save_img, save_labels = make_sub_dirctory_save(prefix, save_path)
+
     print(f"Looking for {txt_name}")
     if os.path.exists(txt_name):
         print(f'cutting image {i+1}/{len(imglist)}')
         cut(img_name, save_img, name, save_labels, txt_name, i)
+        file_counter += files_to_add
     else:
         print("no text file for image")
 print("done")
